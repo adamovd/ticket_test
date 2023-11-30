b0f80f2344abf64f1c2aaa96a77a594e1327fd71
Adam Danielsson <adam@produktionproduktion.se>
2023-11-30 21:07:41 +0100

    refactor: move out fetch to services

--- app/Services/countryServices.ts
+++ app/Services/countryServices.ts
@@ -1,29 +1,10 @@
 import axios from "axios";
 import { Country } from "../Models/Country";
 
-const createNewCountry = async ({
-  name,
-  description,
-  population,
-  lat,
-  lng,
-  imageUrl,
-  playlistUrl,
-}: Country) => {
-  let response = await axios.post<Country>(
-    "https://www.eattheworld.se/api/countries",
-    {
-      name: name,
-      description: description,
-      population: population,
-      lat: lat,
-      lng: lng,
-      imageUrl: imageUrl,
-      recipes: [],
-      playlistUrl: playlistUrl,
-    }
-  );
-  return response.data;
+export const createNewCountry = async ({ ...data }: Country) => {
+  fetch("https://www.eattheworld.se/api/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ...data }),
+  });
 };
-
-export default createNewCountry;
