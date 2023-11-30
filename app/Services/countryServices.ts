dba06eb0e0289af69ce688149a4a34c2764d50a6
Adam Danielsson <adam@produktionproduktion.se>
2023-11-30 21:18:21 +0100

    fix: spelling

--- app/Services/countryServices.ts
+++ app/Services/countryServices.ts
@@ -1,7 +1,7 @@
 import { Country } from "../Models/Country";
 
 const createNewCountry = async (data: Country) => {
-  fetch("https://www.eattheworld.se/api/register", {
+  fetch("https://www.eattheworld.se/api/countries", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ ...data }),
