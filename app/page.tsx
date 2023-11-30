3e8514eb33250c809dcd5ae303d0998deff00bb1
Adam Danielsson <adam@produktionproduktion.se>
2023-11-30 23:06:57 +0100

    feat: show countries

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getAllCountries } from "./Services/countryServices";
+import { Country } from "./Models/Country";
 
 const Home = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
   useEffect(() => {
     getAllCountries().then((response) => {
-      console.log(response.json());
+      console.log(response);
+      setCountries(response);
     });
   });
   return (
     <>
       <h1>Home</h1>
+      {countries.map((country, index) => {
+        <>
+          <h3>{country.name}</h3>
+          <p>{country.description}</p>
+        </>;
+      })}
     </>
   );
 };
