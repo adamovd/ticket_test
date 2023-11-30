aa45e9216d672686070cefcd283dfa75a5354ddf
Adam Danielsson <adam@produktionproduktion.se>
2023-11-30 17:20:12 +0100

    fix: might work without https

--- next.config.cjs
+++ next.config.cjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    dbConfig: {
-      host: "eattheworld.se.mysql",
-      port: 3306,
-      user: "eattheworld_secountrydb",
-      password: "admin123",
-      database: "eattheworld_secountrydb",
-    },
-    secret: "7e027292-3935-46b1-a04e-c096ac38b7ef",
-  },
-  publicRuntimeConfig: {
-    apiUrl:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/api" // development api
-        : "http://eattheworld.se/api", // production api
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
