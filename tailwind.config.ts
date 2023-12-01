827830c52b9f9e22b3f597d913475e86b7a732d7
Adam Danielsson <adam@produktionproduktion.se>
2023-12-01 10:22:49 +0100

    fix: packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,5 +26,8 @@ export default withUt({
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 });
